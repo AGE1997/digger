c94b8dc11b224d862cbec889ba2e615878cafc25
AGE1997 <91370795+AGE1997@users.noreply.github.com>
2022-03-13 20:39:31 +0900

    Revert "ユーザー管理機能2"

--- app/controllers/profiles_controller.rb
+++
@@ -1,6 +0,0 @@
-class ProfilesController < ApplicationController
-  def profile_params
-    params.require(:profile).permit(:nickname, :sex_id, :genre_id, :prefecture_id, :birthday, :biography,
-                                    :image).merge(user_id: current_user.id)
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
-  devise_scope :user do
-    get 'profiles', to: 'users/registrations#new_profile'
-    post 'profiles', to: 'users/registrations#create_profile'
-  end
+  devise_for :users
   get 'videos/index'
   root to: "videos#index"
 end
